c1f5b6ec07120240f29557566ee9be258e1eda1c
M-Kathae <160848206+M-Kathae@users.noreply.github.com>
2024-03-07 13:53:33 +0300

    Add files via upload

---
+++ tasks.dart
@@ -0,0 +1,29 @@
+// Task 1
+num addTwo(num a, num b) {
+  return a + b;
+}
+
+// Task 2
+num subtractTwo(num a, num b) {
+  return a - b;
+}
+
+// Task 3
+num multiplyTwo(num a, num b) {
+  return a * b;
+}
+
+// Task 4
+double divideTwo(num a, num b) {
+  return a / b;
+}
+
+// Task 5
+int stringLength(String str) {
+  return str.length;
+}
+
+// Task 6
+dynamic getFirstElement(List list) {
+  return list[0];
+}
